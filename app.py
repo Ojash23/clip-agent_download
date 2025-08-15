e4efaa3d38a99b00a8a8a780b6893d59429c9790
Ojash Prasad <66564131+Ojash23@users.noreply.github.com>
2025-08-16 05:02:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -560,4 +560,5 @@ def download_clip():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=True)
+     port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
